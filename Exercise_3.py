87c7032f1f05ed030ebb471a0d2170c662b2a01c
pratikb0501 <pratikbagmare@gmail.com>
2026-04-12 18:43:45 -0700

    Precourse 2

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,31 @@
 # Node class  
-class Node:  
-  
+class Node:
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data,next=None):
+        self.data = data
+        self.next = next
         
 class LinkedList: 
   
     def __init__(self): 
-        
+        self.head = Node(-1)
   
-    def push(self, new_data): 
+    def push(self, new_data):
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        newNode = Node(new_data)
+        temp.next = newNode
         
   
     # Function to get the middle of  
     # the linked list 
-    def printMiddle(self): 
+    def printMiddle(self):
+        slow,fast = self.head.next,self.head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
 
 # Driver code 
 list1 = LinkedList() 
--- Exercise_5.py
+++ Exercise_5.py
@@ -2,9 +2,37 @@
 
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
-  #write your code here
+    pivot = arr[h]
+    i = l - 1
+
+    for j in range(l, h):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[h] = arr[h], arr[i + 1]
+    return i + 1
 
 
 def quickSortIterative(arr, l, h):
-  #write your code here
+  # Create an explicit stack
+  stack = []
+
+  # push initial range
+  stack.append((l, h))
+
+  # Process stack until empty
+  while stack:
+      l, h = stack.pop()
+
+      # Partition the array
+      pi = partition(arr, l, h)
+
+      # If elements on left side of pivot, push left side
+      if pi - 1 > l:
+          stack.append((l, pi - 1))
+
+      # If elements on right side of pivot, push right side
+      if pi + 1 < h:
+          stack.append((pi + 1, h))
 
